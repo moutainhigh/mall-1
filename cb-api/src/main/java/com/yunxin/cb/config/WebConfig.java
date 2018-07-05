574d1c4c9fd93641881d432dea864f29360ab547
tanggangyi <tanggangyi@sina.com>
2018-07-05 15:52:50 +0800

    安全框架配置

---
+++ cb-api/src/main/java/com/yunxin/cb/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.yunxin.cb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * @author gonglei
+ * @version 17/11/6 下午3:09
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
