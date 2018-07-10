432e961bb0ad251bdad411af0bf89b31108adeb5
tanggangyi <tanggangyi@sina.com>
2018-07-10 15:02:33 +0800

    富德保险数据同步

---
+++ cb-api/src/main/java/com/yunxin/cb/rest/JsonpAdvice.java
@@ -0,0 +1,16 @@
+package com.yunxin.cb.rest;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+/**
+ * 对使用HttpMessageConverter的@ResponseBody的支持 
+ */
+
+@ControllerAdvice
+public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+    public JsonpAdvice() {
+        super("callback"); //指定jsonpParameterNames
+    }
+}
--- cb-core/src/main/java/com/yunxin/cb/sinolife/SinoLifeService.java
+++ cb-core/src/main/java/com/yunxin/cb/sinolife/SinoLifeService.java
@@ -1,6 +1,7 @@
 package com.yunxin.cb.sinolife;
 
-import com.yunxin.cb.util.HttpsUtils;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SinoLifeService {
 
@@ -9,7 +10,16 @@ public class SinoLifeService {
     private String pwd;
 
     private boolean login(){
-        HttpsUtils.doPost("");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Host","www.sino-life.com");
+        headers.put("Upgrade-Insecure-Requests","1");
+        headers.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+        headers.put("Content-Type","application/x-www-form-urlencoded");
+        headers.put("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36");
+        headers.put("Referer","https://www.sino-life.com/elogin/LOGIN/domain-1/login.html");
+        Map<String, Object> params = new HashMap<>();
+        params.put("toURL","http://www.sino-life.com/eportal/index.do");
+        String response = HttpsUtils.doPost("https://www.sino-life.com/elogin/Login.sso", params, headers);
         return true;
     }
 }
