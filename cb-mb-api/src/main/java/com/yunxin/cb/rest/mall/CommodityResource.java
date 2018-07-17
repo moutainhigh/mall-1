fd5b4d3b66217577db509daa1dc9e6d4d14f4301
tangou <twx211553@163.com>
2018-07-17 18:35:31 +0800

    加入注释

--- cb-mb-api/src/main/java/com/yunxin/cb/rest/mall/CommodityResource.java
+++ cb-mb-api/src/main/java/com/yunxin/cb/rest/mall/CommodityResource.java
@@ -1,54 +1,33 @@
 package com.yunxin.cb.rest.mall;
 
-
-import com.yunxin.cb.mall.entity.Commodity;
 import com.yunxin.cb.rest.BaseResource;
 import com.yunxin.cb.vo.ResponseResult;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @title: 商城商品接口
+ * @auther: eleven
+ * @date: 2018/7/17 18:29
+ */
 @Api(description = "商城商品接口")
 @RestController
 @RequestMapping(value = "/mall/commodity")
 @SessionAttributes("customerId")
 public class CommodityResource extends BaseResource {
 
+    /**
+     * @title: 通过商品ID查询商品详情
+     * @param: [commodityId]
+     * @return: com.yunxin.cb.vo.ResponseResult
+     * @auther: eleven
+     * @date: 2018/7/17 18:29
+     */
     @ApiOperation(value = "通过商品ID查询商品详情")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "commodityId", value = "商品ID", required = true, paramType = "path", dataType = "int")
-    })
+            @ApiImplicitParam(name = "commodityId", value = "商品ID", required = true, paramType = "path", dataType = "int")})
     @GetMapping(value = "getCommdity/{commodityId}")
     public ResponseResult getCommdity(@PathVariable(value = "commodityId") int commodityId){
         return new ResponseResult(null);
     }
-
-    @ApiOperation(value = "获取用户收藏夹")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "customerId", value = "客户ID", required = true, paramType = "post", dataType = "int")
-    })
-    @PostMapping(value = "getCustomerFavorite")
-    public ResponseResult getCustomerFavorite(@RequestParam(value = "customerId") int commodityId){
-        return new ResponseResult(null);
-    }
-
-    @ApiOperation(value = "商品添加收藏夹")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "commodityId", value = "商品ID", required = true, paramType = "post", dataType = "int"),
-            @ApiImplicitParam(name = "customerId", value = "客户ID", required = true, paramType = "post", dataType = "int")
-    })
-    @PostMapping(value = "addFavorite")
-    public ResponseResult addFavorite(@RequestParam(value = "commodityId") int commodityId,@RequestParam(value = "customerId") int customerId){
-        return new ResponseResult(null);
-    }
-
-    @ApiOperation(value = "商品移出收藏夹")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "commodityId", value = "商品ID", required = true, paramType = "post", dataType = "int"),
-            @ApiImplicitParam(name = "customerId", value = "客户ID", required = true, paramType = "post", dataType = "int")
-    })
-    @PostMapping(value = "delFavorite")
-    public ResponseResult delFavorite(@RequestParam(value = "commodityId") int commodityId,@RequestParam(value = "customerId") int customerId){
-        return new ResponseResult(null);
-    }
-
 }
