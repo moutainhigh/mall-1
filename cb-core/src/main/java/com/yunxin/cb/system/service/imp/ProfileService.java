32ffb43501c15f94d8efe0451166e5f7f0a75039
likang <3499384@qq.com>
2018-07-20 17:49:36 +0800

    系统配置修改。profileName翻译，上传加个加载框

--- cb-core/src/main/java/com/yunxin/cb/system/service/imp/ProfileService.java
+++ cb-core/src/main/java/com/yunxin/cb/system/service/imp/ProfileService.java
@@ -32,19 +32,20 @@ public class ProfileService implements IProfileService {
 
     /**
      * 根据手机系统类型查询APP版本信息
+     *
      * @param mobileOSType
      * @return
      */
     @Override
-    public AppCheckUpdate getAppCheckUpdate(MobileOSType mobileOSType){
-        if(mobileOSType == MobileOSType.ANDROID){
+    public AppCheckUpdate getAppCheckUpdate(MobileOSType mobileOSType) {
+        if (mobileOSType == MobileOSType.ANDROID) {
             String v = profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_CODE).getFileValue();
-            int versionCode = StringUtils.isNotBlank(v)?Integer.parseInt(v):0;
+            int versionCode = StringUtils.isNotBlank(v) ? Integer.parseInt(v) : 0;
             String versionName = profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_NAME).getFileValue();
             String appName = profileDao.getProfileByProfileName(ProfileName.ANDROID_APP_NAME).getFileValue();
             String url = profileDao.getProfileByProfileName(ProfileName.ANDROID_URL).getFileValue();
             String description = profileDao.getProfileByProfileName(ProfileName.ANDROID_DESCRIPTION).getFileValue();
-            return new AppCheckUpdate(versionCode,versionName, appName, url, description);
+            return new AppCheckUpdate(versionCode, versionName, appName, url, description);
         }
 
         return null;
@@ -53,19 +54,21 @@ public class ProfileService implements IProfileService {
 
     /**
      * 系统配置分页信息
-     * @author      likang
+     *
      * @param query
-     * @return      org.springframework.data.domain.Page<com.yunxin.cb.system.entity.Profile>
-     * @exception
-     * @date        2018/7/19 9:50
+     * @return org.springframework.data.domain.Page<com.yunxin.cb.system.entity.Profile>
+     * @throws
+     * @author likang
+     * @date 2018/7/19 9:50
      */
     @Override
-    public Page<Profile> pageProfile(PageSpecification<Profile> query){
+    public Page<Profile> pageProfile(PageSpecification<Profile> query) {
         query.setCustomSpecification(new CustomSpecification<Profile>() {
             @Override
             public void buildFetch(Root<Profile> root) {
 
             }
+
             @Override
             public void addConditions(Root<Profile> root, CriteriaQuery<?> query,
                                       CriteriaBuilder builder, List<Predicate> predicates) {
@@ -77,22 +80,23 @@ public class ProfileService implements IProfileService {
 
     /**
      * 添加系统配置
-     * @author      likang
-     * @return      com.yunxin.cb.system.entity.Profile
-     * @exception
-     * @date        2018/7/19 10:14
+     *
+     * @return com.yunxin.cb.system.entity.Profile
+     * @throws
+     * @author likang
+     * @date 2018/7/19 10:14
      */
     @Override
     @Transactional
-    public Profile addProfile(){
+    public Profile addProfile() {
         //如果不存在ANDROID的系统配置，直接添加
-        if(null==profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_CODE)){
-            profileDao.save(new Profile(ProfileName.ANDROID_VERSION_CODE,ProfileName.ANDROID_VERSION_CODE.getDefaultValue()));
-            profileDao.save(new Profile(ProfileName.ANDROID_VERSION_NAME,ProfileName.ANDROID_VERSION_NAME.getDefaultValue()));
-            profileDao.save(new Profile(ProfileName.ANDROID_APP_NAME,ProfileName.ANDROID_APP_NAME.getDefaultValue()));
-            profileDao.save(new Profile(ProfileName.ANDROID_URL,ProfileName.ANDROID_URL.getDefaultValue()));
-            profileDao.save(new Profile(ProfileName.ANDROID_DESCRIPTION,ProfileName.ANDROID_DESCRIPTION.getDefaultValue()));
-        }else{
+        if (null == profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_CODE)) {
+            profileDao.save(new Profile(ProfileName.ANDROID_VERSION_CODE, ProfileName.ANDROID_VERSION_CODE.getDefaultValue()));
+            profileDao.save(new Profile(ProfileName.ANDROID_VERSION_NAME, ProfileName.ANDROID_VERSION_NAME.getDefaultValue()));
+            profileDao.save(new Profile(ProfileName.ANDROID_APP_NAME, ProfileName.ANDROID_APP_NAME.getDefaultValue()));
+            profileDao.save(new Profile(ProfileName.ANDROID_URL, ProfileName.ANDROID_URL.getDefaultValue()));
+            profileDao.save(new Profile(ProfileName.ANDROID_DESCRIPTION, ProfileName.ANDROID_DESCRIPTION.getDefaultValue()));
+        } else {
             profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_CODE).setFileValue(ProfileName.ANDROID_VERSION_CODE.getDefaultValue());
             profileDao.getProfileByProfileName(ProfileName.ANDROID_VERSION_NAME).setFileValue(ProfileName.ANDROID_VERSION_NAME.getDefaultValue());
             profileDao.getProfileByProfileName(ProfileName.ANDROID_APP_NAME).setFileValue(ProfileName.ANDROID_APP_NAME.getDefaultValue());
@@ -100,5 +104,36 @@ public class ProfileService implements IProfileService {
             profileDao.getProfileByProfileName(ProfileName.ANDROID_DESCRIPTION).setFileValue(ProfileName.ANDROID_DESCRIPTION.getDefaultValue());
         }
         return null;
+    }
+
+    /**
+     * 获取Profile详情
+     *
+     * @param fileId
+     * @return com.yunxin.cb.system.entity.Profile
+     * @throws
+     * @author likang
+     * @date 2018/7/20 16:42
+     */
+    @Override
+    public Profile getProfile(int fileId) {
+        return profileDao.findOne(fileId);
+    }
+
+    /**
+     * 修改参数配置
+     *
+     * @param profile
+     * @return com.yunxin.cb.system.entity.Profile
+     * @throws
+     * @author likang
+     * @date 2018/7/20 16:44
+     */
+    @Override
+    @Transactional
+    public Profile updateProfile(Profile profile) {
+        Profile oldProfile = profileDao.findOne(profile.getFileId());
+        oldProfile.setFileValue(profile.getFileValue());
+        return oldProfile;
     }
 }
