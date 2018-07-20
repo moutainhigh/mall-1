673be0cf3b78d113177ca237c52d28d55953b20a
guwenshao <294727977@qq.com>
2018-07-20 12:02:03 +0800

    退货查询

--- cb-mb-api/src/main/java/com/yunxin/cb/rest/mall/ProductReturnResource.java
+++ cb-mb-api/src/main/java/com/yunxin/cb/rest/mall/ProductReturnResource.java
@@ -5,15 +5,13 @@ import com.yunxin.cb.mall.entity.ProductReturn;
 import com.yunxin.cb.mall.service.ProductReturnService;
 import com.yunxin.cb.meta.Result;
 import com.yunxin.cb.rest.BaseResource;
+import com.yunxin.cb.util.page.Query;
 import com.yunxin.cb.vo.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -30,18 +28,39 @@ public class ProductReturnResource extends BaseResource {
             @ApiImplicitParam(name = "orderId", value = "订单id", required = true, paramType = "post", dataType = "int"),
             @ApiImplicitParam(name = "itemId", value = "订单商品id", required = true, paramType = "post", dataType = "int"),
             @ApiImplicitParam(name = "reason", value = "退货原因", paramType = "post", dataType = "String")})
-    @PostMapping(value = "saveGoodsReturn")
-    public ResponseResult saveGoodsReturn(@RequestBody ProductReturn productReturn) throws Exception{
+    @PostMapping(value = "applyOrderProductReturn")
+    public ResponseResult applyOrderProductReturn(@RequestBody ProductReturn productReturn) throws Exception{
+        productReturn.setCustomerId(getCustomerId());
         productReturnService.applyOrderProductReturn(productReturn);
         return new ResponseResult(Result.SUCCESS);
     }
 
-    @ApiOperation(value = "退货查询")
+    @ApiOperation(value = "退货分页列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "goodsReturn", value = "退货查询对象", required = true, paramType = "post", dataType = "Object")})
-    @PostMapping(value = "getGoodsReturn")
-    public ResponseResult getGoodsReturn(@RequestBody ProductReturn productReturn){
-        return new ResponseResult(null);
+            @ApiImplicitParam(name = "orderId", value = "订单id", paramType = "post", dataType = "int")})
+    @PostMapping(value = "pageProductReturn")
+    public ResponseResult pageProductReturn(Query q, @RequestBody ProductReturn productReturn){
+        q.setData(productReturn);
+        return new ResponseResult(productReturnService.pageProductReturn(q));
+    }
+
+    @ApiOperation(value = "退货列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "orderId", value = "订单id", paramType = "post", dataType = "int")})
+    @PostMapping(value = "listProductReturn")
+    public ResponseResult listProductReturn(@RequestBody ProductReturn productReturn){
+        Query q = new Query();
+        q.setData(productReturn);
+        return new ResponseResult(productReturnService.listProductReturn(q));
+    }
+
+    @ApiOperation(value = "退货详情")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "productReturnId", value = "退货id", required = true, paramType = "post", dataType = "int")})
+    @PostMapping(value = "getProductReturn")
+    public ResponseResult getProductReturn(@RequestParam Integer productReturnId){
+        ProductReturn productReturn = productReturnService.getProductReturn(productReturnId, getCustomerId());
+        return new ResponseResult(productReturn);
     }
 
 
