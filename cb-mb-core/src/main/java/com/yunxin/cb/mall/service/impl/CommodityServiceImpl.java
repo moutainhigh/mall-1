cc0767ed4d2a8961d3113861e4be705c7be60b5b
tangou <twx211553@163.com>
2018-07-21 16:18:18 +0800

    查找商品图片并返回

--- cb-mb-core/src/main/java/com/yunxin/cb/mall/service/impl/CommodityServiceImpl.java
+++ cb-mb-core/src/main/java/com/yunxin/cb/mall/service/impl/CommodityServiceImpl.java
@@ -1,6 +1,9 @@
 package com.yunxin.cb.mall.service.impl;
 
-import com.yunxin.cb.mall.entity.*;
+import com.yunxin.cb.mall.entity.Commodity;
+import com.yunxin.cb.mall.entity.CommoditySpec;
+import com.yunxin.cb.mall.entity.Favorite;
+import com.yunxin.cb.mall.entity.Product;
 import com.yunxin.cb.mall.entity.meta.PaymentType;
 import com.yunxin.cb.mall.entity.meta.ProductState;
 import com.yunxin.cb.mall.entity.meta.PublishState;
@@ -14,8 +17,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FilenameFilter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,12 +69,6 @@ public class CommodityServiceImpl implements CommodityService {
         paymentMap.put(PaymentType.FULL_SECTION.ordinal(),PaymentType.FULL_SECTION.getName());
         paymentMap.put(PaymentType.LOAN.ordinal(),PaymentType.LOAN.toString());
         resultMap.put("paymentType", paymentMap);//支付方式
-//        List<AttributeGroup> attributeGroups = attributeGroupMapper.getAttributeGroupsByCommodityId(commodity.getCommodityId());
-//        Map goodAttrMap = new HashMap();//商品属性Map
-//        for (AttributeGroup attributeGroup:attributeGroups){
-//            goodAttrMap.put(attributeGroup.getGroupName(),attributeGroup.getAttributes());
-//        }
-//        resultMap.put("attributeGroups", goodAttrMap);//属性组及属性
         if(customerId>0){//用户存在则查询商品收藏夹
             Favorite favorite=new Favorite();
             favorite.setCustomerId(customerId);
@@ -94,27 +89,5 @@ public class CommodityServiceImpl implements CommodityService {
     public List<Product> getProductsByCommodityId(int commodityId) {
         List<Product> products = productMapper.selectAllByCommodityId(commodityId);
         return products;
-    }
-
-    /**
-     * @title: 获取原来的图
-     * @param: [imagesDir, commodity]
-     * @return: java.lang.String[]
-     * @auther: eleven
-     * @date: 2018/7/20 10:15
-     */
-    private String[] getImagePath(String imagesDir, Commodity commodity) {
-
-        File imageDir = new File(imagesDir + commodity.getCommodityCode());
-        String[] images = imageDir.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                if (name.endsWith("jpg")) {
-                    return true;
-                }
-                return false;
-            }
-        });
-        return images;
     }
 }
