339030e252838df7c53c2a1abed5323ed5c40f26
mwj <mwj545210@163.com>
2018-07-21 11:15:00 +0800

    mall 修改

--- app-mall/src/router/index.js
+++ app-mall/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
 import CarList from '../page/carList/CarList'
+import Home from '../page/home/Home'
 
 Vue.use(Router)
 
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     },
     {
       path: '/car-list',
