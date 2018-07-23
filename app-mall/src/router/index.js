0ba5296e9c76ef2009262fe23867777f62006c1a
weiyinhong <1169369084@qq.com>
2018-07-23 19:47:21 +0800

    modify

--- app-mall/src/router/index.js
+++ app-mall/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import CarList from '../page/carList/CarList'
 import CarDetail from '../page/carDetail/CarDetail'
+import OrderComfirm from '../page/order/OrderComfirm'
 import Home from '../page/home/Home'
 import Search from '../page/search/Search'
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/car-detail',
       name: 'CarDetail',
       component: CarDetail
+    },
+    {
+      path: '/order-comfirm',
+      name: 'OrderComfirm',
+      component: OrderComfirm
     }
   ]
 })
