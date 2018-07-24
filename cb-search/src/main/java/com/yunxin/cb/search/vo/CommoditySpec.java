dccba4784812c460bef368e08f06c9ae211ad720
tanggangyi <tanggangyi@sina.com>
2018-07-24 20:12:04 +0800

    搜索

---
+++ cb-search/src/main/java/com/yunxin/cb/search/vo/CommoditySpec.java
@@ -0,0 +1,27 @@
+package com.yunxin.cb.search.vo;
+
+/**
+ * 商品规格
+ */
+public class CommoditySpec implements java.io.Serializable {
+
+    private String specName;
+
+    private String value;
+
+    public String getSpecName() {
+        return specName;
+    }
+
+    public void setSpecName(String specName) {
+        this.specName = specName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
