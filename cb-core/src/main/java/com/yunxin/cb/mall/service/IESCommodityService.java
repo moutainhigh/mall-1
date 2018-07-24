9db99fbee456ac650feb6c0ba1227e67ec0b428e
pengcong <cong18679307314>
2018-07-24 15:40:38 +0800

    更新接口

--- cb-core/src/main/java/com/yunxin/cb/mall/service/IESCommodityService.java
+++ cb-core/src/main/java/com/yunxin/cb/mall/service/IESCommodityService.java
@@ -3,19 +3,9 @@
  */
 package com.yunxin.cb.mall.service;
 
-import com.yunxin.cb.mall.entity.*;
-import com.yunxin.cb.mall.entity.meta.CommodityState;
-import com.yunxin.cb.mall.entity.meta.PublishState;
-import com.yunxin.cb.mall.query.CommodityQuery;
-import com.yunxin.cb.mall.vo.Commodity;
-import com.yunxin.core.exception.EntityExistException;
-import com.yunxin.core.persistence.PageSpecification;
-import org.springframework.data.domain.Page;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import com.yunxin.cb.mall.entity.CommodityEs;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author gonglei
