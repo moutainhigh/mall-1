8eb9be7712d2575d9dfe61f5621e86617bc5c224
liuxiaochang <qwer1234>
2018-08-07 10:52:50 +0800

    资金池相关类

--- cb-core/src/main/java/com/yunxin/cb/rb/dao/FundsPoolDao.java
+++ cb-core/src/main/java/com/yunxin/cb/rb/dao/FundsPoolDao.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
+ * 
  * @author lxc
  */
 public interface FundsPoolDao extends JpaRepository<FundsPool, Integer>, JpaSpecificationExecutor<FundsPool> {
