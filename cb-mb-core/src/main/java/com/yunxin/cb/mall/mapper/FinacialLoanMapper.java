00385886951706f61c9a24a84468bd5fba51731f
likang <3499384@qq.com>
2018-08-09 16:18:36 +0800

    1

--- cb-mb-core/src/main/java/com/yunxin/cb/mall/mapper/FinacialLoanMapper.java
+++ cb-mb-core/src/main/java/com/yunxin/cb/mall/mapper/FinacialLoanMapper.java
@@ -76,8 +76,7 @@ public interface FinacialLoanMapper {
             "LOAN_ID, CUSTOMER_ID, AMOUNT, TERM, INTEREST_RATE, TYPE, REPAY_DAY, STATE, CREATE_TIME, ",
             "UPDATE_TIME, REPAYMENT_TERM, FINAL_REPAYMENT_TIME, REPAY_AMOUNT, READY_AMOUNT, ",
             "SURPLUS_AMOUNT, LATE_FEE, INTEREST, OVERDUE_NUMER, BANK_ID",
-            "from finacial_loan where CUSTOMER_ID = #{customerId,jdbcType=INTEGER} ",
-            "order by CREATE_TIME asc"
+            "from finacial_loan where CUSTOMER_ID = #{customerId,jdbcType=INTEGER}"
     })
     @Results({
             @Result(column="LOAN_ID", property="loanId", jdbcType=JdbcType.INTEGER, id=true),
@@ -100,7 +99,7 @@ public interface FinacialLoanMapper {
             @Result(column="OVERDUE_NUMER", property="overdueNumer", jdbcType=JdbcType.INTEGER),
             @Result(column="BANK_ID", property="bankId", jdbcType=JdbcType.INTEGER)
     })
-    List<FinacialLiabilitiesBill> selectByCustomerId(int customerId);
+    List<FinacialLoan> selectByCustomerId(int customerId);
 
     @Update({
             "update finacial_loan",
