19b43ff4e2f7e8b6f29a53ad26526169d1f509a2
tangou <twx211553@163.com>
2018-08-13 09:37:36 +0800

    加入保险返现日志记录

---
+++ cb-core/src/main/java/com/yunxin/cb/mall/entity/meta/WithdrawState.java
@@ -0,0 +1,39 @@
+package com.yunxin.cb.mall.entity.meta;
+
+/**
+ * @title: 提现状态枚举类
+ * @auther: eleven
+ * @date: 2018/8/9 10:15
+ */
+public enum WithdrawState {
+
+    AUDIT("审核中",0), AUDIT_NOT("审核失败",1), WAIT_GRANT("待发放",2), TRANSFER("转账中",3), FANISHED("交易完成",4);
+    private String name;
+    private Integer value;
+
+    private WithdrawState(String name,Integer value) {
+        this.name = name;
+        this.value=value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
