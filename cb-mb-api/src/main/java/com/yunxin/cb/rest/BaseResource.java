60a80db9a0f148885f915abecbec1c35019a8f6b
liuxiaochang <qwer1234>
2018-08-16 16:47:10 +0800

    /**      * 得到一个完整URL（包含参数）      * @param request      * @param urlIsTrue     是否包含URL,true包含,false不包含      * @return      */     public static String getFullUrlIsTrue(HttpServletRequest request,boolean urlIsTrue) {         StringBuffer url = new StringBuffer();         int i = 0;         if(urlIsTrue) {             url.append(request.getRequestURL());         }         Enumeration paramNames = request.getParameterNames();         while (paramNames.hasMoreElements()) {             String key = (String) paramNames.nextElement();             String[] paramValues = request.getParameterValues(key);             if (paramValues.length == 1) {                 String paramValue = paramValues[0];                 //if (paramValue.length() != 0) {                 if (i++ == 0) {                     url.append("?" + key + "=" + paramValue);                 } else {                     url.append("&" + key + "=" + paramValue);                 }                 //}             }         }         return url.toString();     }
        /**
         * 得到一个完整URL（包含参数）
         * @param request
         * @return
         */
        public static String getFullUrl(HttpServletRequest request) {
            return getFullUrlIsTrue(request,true);
        }
    
        /**
         * 得到一个完整请求参数
         * @param request
         * @return
         */
        public static String getFullParam(HttpServletRequest request) {
            return getFullUrlIsTrue(request,false);
        }

--- cb-mb-api/src/main/java/com/yunxin/cb/rest/BaseResource.java
+++ cb-mb-api/src/main/java/com/yunxin/cb/rest/BaseResource.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import com.yunxin.cb.orm.CustomerContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 public class BaseResource {
@@ -37,5 +38,54 @@ public class BaseResource {
             ip = request.getRemoteAddr();
         }
         return ip;
+    }
+
+
+    /**
+     * 得到一个完整URL（包含参数）
+     * @param request
+     * @param urlIsTrue     是否包含URL,true包含,false不包含
+     * @return
+     */
+    public static String getFullUrlIsTrue(HttpServletRequest request,boolean urlIsTrue) {
+        StringBuffer url = new StringBuffer();
+        int i = 0;
+        if(urlIsTrue) {
+            url.append(request.getRequestURL());
+        }
+        Enumeration paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String key = (String) paramNames.nextElement();
+            String[] paramValues = request.getParameterValues(key);
+            if (paramValues.length == 1) {
+                String paramValue = paramValues[0];
+                //if (paramValue.length() != 0) {
+                if (i++ == 0) {
+                    url.append("?" + key + "=" + paramValue);
+                } else {
+                    url.append("&" + key + "=" + paramValue);
+                }
+                //}
+            }
+        }
+        return url.toString();
+    }
+
+    /**
+     * 得到一个完整URL（包含参数）
+     * @param request
+     * @return
+     */
+    public static String getFullUrl(HttpServletRequest request) {
+        return getFullUrlIsTrue(request,true);
+    }
+
+    /**
+     * 得到一个完整请求参数
+     * @param request
+     * @return
+     */
+    public static String getFullParam(HttpServletRequest request) {
+        return getFullUrlIsTrue(request,false);
     }
 }
