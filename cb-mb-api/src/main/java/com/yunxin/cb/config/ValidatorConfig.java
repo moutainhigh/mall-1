e9ad5ff105b2e02ac62647462652b9200844be7d
guwenshao <294727977@qq.com>
2018-08-21 15:28:11 +0800

    验证优化

---
+++ cb-mb-api/src/main/java/com/yunxin/cb/config/ValidatorConfig.java
@@ -0,0 +1,31 @@
+package com.yunxin.cb.config;
+
+import org.hibernate.validator.HibernateValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * @author guwenshao
+ */
+@Configuration
+public class ValidatorConfig {
+
+    /**
+     * 配置快速失败模式
+     * @return
+     */
+    @Bean
+    public Validator validator(){
+        ValidatorFactory validatorFactory = Validation.byProvider( HibernateValidator.class )
+                .configure()
+                .addProperty( "hibernate.validator.fail_fast", "true" )
+                .buildValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        return validator;
+    }
+}
