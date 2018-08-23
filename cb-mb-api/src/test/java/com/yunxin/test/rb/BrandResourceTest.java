b797679cff7180bcd48159b9765735f130caac06
guwenshao <294727977@qq.com>
2018-08-23 12:23:21 +0800

    测试用例优化

--- cb-mb-api/src/test/java/com/yunxin/test/rb/BrandResourceTest.java
+++ cb-mb-api/src/test/java/com/yunxin/test/rb/BrandResourceTest.java
@@ -3,17 +3,13 @@ package com.yunxin.test.rb;
 import com.yunxin.cb.rest.mall.BrandResource;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 public class BrandResourceTest extends MockHttpUtils{
-    private static final Logger log = LoggerFactory.getLogger(BrandResourceTest.class);
     @Autowired
     private BrandResource brandResource;
     @Before
@@ -24,7 +20,7 @@ public class BrandResourceTest extends MockHttpUtils{
     public void getBrand() throws Exception {
         log.info("查询所有品牌 V1 start");
         String url = "/v1/mall/brand/list";
-        Map<String,Object> map = new HashMap<>();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
         commonMvcPerFormGet(url,"",MediaType.APPLICATION_JSON_VALUE,"SUCCESS",map);
 
     }
@@ -32,7 +28,7 @@ public class BrandResourceTest extends MockHttpUtils{
     public void getHotBrand() throws Exception {
         log.info("查询所有品牌 V1 start");
         String url = "/v1/mall/brand/list";
-        Map<String,Object> map = new HashMap<>();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
         commonMvcPerFormPost(url,"",MediaType.APPLICATION_JSON_VALUE,"SUCCESS",map);
 
     }
