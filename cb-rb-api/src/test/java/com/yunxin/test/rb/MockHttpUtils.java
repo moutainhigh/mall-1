8e0f98a68c318c988f86507bddd040a2f6a2d85d
pengcong <cong18679307314>
2018-08-29 11:04:54 +0800

    报账mock测试demo

--- cb-rb-api/src/test/java/com/yunxin/test/rb/MockHttpUtils.java
+++ cb-rb-api/src/test/java/com/yunxin/test/rb/MockHttpUtils.java
@@ -1,13 +1,18 @@
 package com.yunxin.test.rb;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yunxin.cb.Application;
+import com.yunxin.cb.jwt.JwtUtil;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +20,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Map;
 
@@ -31,59 +39,120 @@ public class MockHttpUtils {
 
     private static final Logger log = LoggerFactory.getLogger(MockHttpUtils.class);
 
+    @Autowired
+    protected WebApplicationContext context;
+
+    private final String token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJtb2JpbGUiOiIxMzQwNzE1MTUwNiIsImV4cCI6MTUzNjExNjQ5OCwianRpIjoiMzA2In0.qe35nHUZdu804DS3R9YaCE4c0Cm29ZKMhPiEhvOjBb149NY43OSp3Z2dmx5f7UgxbNKMzrOFDnXwGwk06w-6LQ";
+
     protected MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        //MockMvcBuilders使用构建MockMvc对象   （项目拦截器有效）
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     /**
      * 功能描述: POST请求
      *
      * @param url 请求路径
-     * @param paramMap 输入参数
+     * @param content json参数
      * @param contentType 数据格式
      * @param acceptStatus 期望请求结果码
+     * @param params 表单参数
      * @return:
      * @auther: yangzhen
      * @date: 2018/8/22 17:47
      */
     public void commonMvcPerFormPost(String url,
-                                      Map<String,String> paramMap, String contentType, int acceptStatus)throws Exception{
-        commonMvcPerForm(null,url,paramMap,contentType,acceptStatus);
+                                     String content, String contentType, Object acceptStatus, MultiValueMap<String, String> params)throws Exception{
+        commonMvcPerForm(HttpMethod.POST, url, content, contentType, acceptStatus, params);
     }
 
     /**
      * 功能描述: GET请求
      *
      * @param url 请求路径
-     * @param paramMap 输入参数
+     * @param content json参数
      * @param contentType 数据格式
      * @param acceptStatus 期望请求结果码
+     * @param params 表单参数
      * @return:
      * @auther: yangzhen
      * @date: 2018/8/22 17:47
      */
     public void commonMvcPerFormGet(String url,
-                                     Map<String,String> paramMap,String contentType,int acceptStatus)throws Exception{
-        commonMvcPerForm("get",url,paramMap,contentType,acceptStatus);
+                                    String content,String contentType,Object acceptStatus,MultiValueMap<String, String> params)throws Exception{
+        commonMvcPerForm(HttpMethod.GET, url, content, contentType, acceptStatus, params);
+    }
+    /**
+     * 功能描述: PUT请求
+     *
+     * @param url 请求路径
+     * @param content json参数
+     * @param contentType 数据格式
+     * @param acceptStatus 期望请求结果码
+     * @param params 表单参数
+     * @return:
+     * @auther: yangzhen
+     * @date: 2018/8/22 17:47
+     */
+    public void commonMvcPerFormPut(String url,
+                                    String content,String contentType,Object acceptStatus,MultiValueMap<String, String> params)throws Exception{
+        commonMvcPerForm(HttpMethod.PUT, url, content, contentType, acceptStatus, params);
+
+    }
+    /**
+     * 功能描述: DELETE请求
+     *
+     * @param url 请求路径
+     * @param content json参数
+     * @param contentType 数据格式
+     * @param acceptStatus 期望请求结果码
+     * @param params 表单参数
+     * @return:
+     * @auther: yangzhen
+     * @date: 2018/8/22 17:47
+     */
+    public void commonMvcPerFormDelete(String url,
+                                       String content,String contentType,Object acceptStatus, MultiValueMap<String, String> params)throws Exception{
+        commonMvcPerForm(HttpMethod.DELETE, url, content, contentType, acceptStatus, params);
+
     }
 
-    public void commonMvcPerForm(String perFormType,String url,
-                                  Map<String,String> paramMap,String contentType,int acceptStatus)throws Exception {
+    /**
+     * 功能描述: 请求
+     *
+     * @param url 请求路径
+     * @param content json参数
+     * @param contentType 数据格式
+     * @param acceptStatus 期望请求结果码
+     * @param params 表单参数
+     * @return:
+     * @auther: yangzhen
+     * @date: 2018/8/22 17:47
+     */
+    public void commonMvcPerForm(HttpMethod httpMethod, String url,
+                                 String content, String contentType, Object acceptStatus, MultiValueMap<String, String> params)throws Exception{
         MockHttpServletRequestBuilder requestBuilder;
-        if(StringUtils.isEmpty(perFormType) || "post".equals(perFormType)){
+        if(httpMethod != null){
+            requestBuilder = MockMvcRequestBuilders.request(httpMethod, url);
+        } else{
             requestBuilder = MockMvcRequestBuilders.post(url);
-        }else{
-            requestBuilder = MockMvcRequestBuilders.get(url);
         }
-        if (null != paramMap && !paramMap.isEmpty()){
-            for (String paramName:paramMap.keySet()){
-                requestBuilder.param(paramName,paramMap.get(paramName));
-            }
+        if(StringUtils.isNotBlank(content)){
+            requestBuilder.content(content);
         }
+        requestBuilder.header(JwtUtil.HEADER_STRING, token);
         requestBuilder.contentType(contentType);
-
+        if (params != null && !params.isEmpty()) {
+            requestBuilder.params(params);
+        }
         MvcResult mvcResult = mvc.perform(requestBuilder)
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
-        Assert.assertEquals(acceptStatus, mvcResult.getResponse().getStatus());
+        JSONObject resultJson =  JSONObject.parseObject(mvcResult.getResponse().getContentAsString());
+        Assert.assertEquals(acceptStatus, resultJson.get("result"));
         log.info("响应内容：" + mvcResult.getResponse().getContentAsString());
     }
 
--- cb-rb-api/src/test/java/com/yunxin/test/rb/ReimbursementTest.java
+++ cb-rb-api/src/test/java/com/yunxin/test/rb/ReimbursementTest.java
@@ -1,52 +1,26 @@
 package com.yunxin.test.rb;
 
-import com.yunxin.cb.rest.rb.ReimbursementQueryResource;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 
 public class ReimbursementTest extends MockHttpUtils{
 
     private static final Logger log = LoggerFactory.getLogger(ReimbursementTest.class);
 
-    @Before
-    public void setUp(){
-        mvc = MockMvcBuilders.standaloneSetup(new ReimbursementQueryResource()).build();
-    }
 
     @Test
-    public void getReimbursementTest() {
+    public void getReimbursementTest() throws Exception {
         log.info("查询可报账列表 V1 start");
         String url = "/v1/reimbursement/getReimbursement";
-        Map<String,String> paramMap = new HashMap<>();
-        paramMap.put("pageNo","1");
-        paramMap.put("pageSize","10");
-        try {
-            commonMvcPerFormPost(url,paramMap,MediaType.APPLICATION_JSON_UTF8_VALUE,200);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        MultiValueMap<String,String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("pageNo","1");
+        paramMap.add("pageSize","10");
+        commonMvcPerFormPost(url,null,MediaType.APPLICATION_JSON_UTF8_VALUE,"SUCCESS",paramMap);
     }
 
-    @Test
-    public void addReimbursementTest(){
-        log.info("查询可报账列表 V1 start");
-        String url = "/v1/reimbursement/addReimbursement";
-        Map<String,String> paramMap = new HashMap<>();
-        paramMap.put("orderId","1");
-        paramMap.put("productId","2");
-        paramMap.put("commodityId","3");
-        try {
-            commonMvcPerFormPost(url,paramMap,MediaType.APPLICATION_JSON_UTF8_VALUE,200);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 }
