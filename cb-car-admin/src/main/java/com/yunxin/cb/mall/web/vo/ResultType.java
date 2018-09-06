fee9fa230b2585a3a11c20e8f640220f3323412b
tangou <twx211553@163.com>
2018-09-06 17:59:49 +0800

    初始化

--- cb-car-admin/src/main/java/com/yunxin/cb/mall/web/vo/ResultType.java
+++
@@ -1,9 +0,0 @@
-package com.yunxin.cb.mall.web.vo;
-
-/**
- * Created by gonglei on 16/2/12.
- */
-public enum ResultType {
-
-    SUCCESS,FAILURE,NOT_LOGIN;
-}
--- cb-car-admin/src/main/java/com/yunxin/cb/meta/Result.java
+++
@@ -1,9 +0,0 @@
-package com.yunxin.cb.meta;
-
-/**
- * Created by gonglei on 16/2/12.
- */
-public enum Result {
-
-    SUCCESS,FAILURE,NOT_LOGIN;
-}
