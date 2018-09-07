3b0cd3e9767816fd7b69d32ac52d2722194136f7
liuxiaochang <qwer1234>
2018-09-07 18:44:14 +0800

    //前端支付状态(1未支付,2已支付,3已确认,4已评估,5已付尾款,6交易完成,7已取消)     UNPAID("未支付"), PAID("已支付"),CONFIRMED("已确认"),ASSESSED("已评估"),PAID_TAIL("已付尾款"),FINISHED("交易完成"),CANCLE("已取消");

--- cb-car-core/src/main/java/com/yunxin/cb/mall/entity/meta/CarOrderAppPaymentStateEnum.java
+++ cb-car-core/src/main/java/com/yunxin/cb/mall/entity/meta/CarOrderAppPaymentStateEnum.java
@@ -1,8 +1,8 @@
 package com.yunxin.cb.mall.entity.meta;
 
 public enum CarOrderAppPaymentStateEnum {
-    //前端支付状态(1未支付,2已支付,3确认提车)
-    UNPAID("未支付"), PAID("已支付"),CONFIRMED_LIFT_CAR("确认提车");
+    //前端支付状态(1未支付,2已支付,3已确认,4已评估,5已付尾款,6交易完成,7已取消)
+    UNPAID("未支付"), PAID("已支付"),CONFIRMED("已确认"),ASSESSED("已评估"),PAID_TAIL("已付尾款"),FINISHED("交易完成"),CANCLE("已取消");
 
 
     private String name;
