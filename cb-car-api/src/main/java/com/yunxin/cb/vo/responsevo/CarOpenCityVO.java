d5fb50f79e379b08bd5ac685e8a69c16695d318a
liuxiaochang <qwer1234>
2018-09-07 15:39:32 +0800

    return webExceptionHandler.handleThrowable(e); //异常统一处理

--- cb-car-api/src/main/java/com/yunxin/cb/vo/responsevo/CarOpenCityVO.java
+++ cb-car-api/src/main/java/com/yunxin/cb/vo/responsevo/CarOpenCityVO.java
@@ -16,14 +16,15 @@ public class CarOpenCityVO implements Serializable {
 	@ApiModelProperty(value="主键id",name="id",example="1")
 	private Integer id;
 	//城市名称
-	@ApiModelProperty(value="城市名称",name="brandName",example="深圳")
+	@ApiModelProperty(value="城市名称",name="cityName",example="深圳")
 	private String cityName;
 	//首字母
-	@ApiModelProperty(value="首字母",name="brandName",example="A")
+	@ApiModelProperty(value="首字母",name="initial",example="A")
 	private String initial;
 	//城市编码
-	@ApiModelProperty(value="城市编码",name="brandName",example="100045")
+	@ApiModelProperty(value="城市编码",name="cityCode",example="100045")
 	private String cityCode;
+
 
 	public Integer getId(){
 		return id;
@@ -59,7 +60,7 @@ public class CarOpenCityVO implements Serializable {
 
 	@Override
 	public String toString() {
-		return "CarOpenCityVo{" +
+		return "CarOpenCityVO{" +
 				"id=" + id +
 				", cityName='" + cityName + '\'' +
 				", initial='" + initial + '\'' +
