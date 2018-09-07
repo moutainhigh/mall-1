4889fd948288b14d8de31b074dde59974fa2492d
liuxiaochang <qwer1234>
2018-09-07 10:26:28 +0800

    模拟app接口 demo

---
+++ cb-car-api/src/main/java/com/yunxin/cb/rest/mall/CarOpenCityController.java
@@ -0,0 +1,39 @@
+package com.yunxin.cb.rest.mall;
+
+import com.yunxin.cb.annotation.ApiVersion;
+import com.yunxin.cb.mall.service.CarOpenCityService;
+import com.yunxin.cb.rest.BaseResource;
+import com.yunxin.cb.vo.ResponseResult;
+import com.yunxin.cb.vo.requestvo.CarOpenCityVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Api(description = "开通城市Api")
+@RestController
+@RequestMapping(value = "/{version}/mall")
+public class CarOpenCityController extends BaseResource {
+    @Resource
+    private CarOpenCityService carOpenCityService;
+
+    @ApiOperation(value = "查询所有开通城市 V1")
+    @ApiImplicitParams({
+    })
+    @GetMapping(value = "opencity/list")
+    @ApiVersion(1)
+
+    public ResponseResult<List<CarOpenCityVo>> getOpenCity() {
+        List<CarOpenCityVo> listVo = new ArrayList<>();
+
+        return new ResponseResult(listVo);
+    }
+
+
+}
